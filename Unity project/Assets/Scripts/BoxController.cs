0bb85dfbea31587d7506de4cecce67d111f5ae7f
Raphael Costa <rapha2706@hotmail.com>
2019-10-29 17:59:04 +0000

    MUITA COISA

---
+++ Unity project/Assets/Scripts/BoxController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Debug.Log(collision.collider.tag);
+        if( collision.gameObject.CompareTag("ball") )
+        {
+            Debug.Log(collision.relativeVelocity);
+            collision.rigidbody.AddForce( collision.relativeVelocity * 100 * -1);
+        }
+    }
+}
