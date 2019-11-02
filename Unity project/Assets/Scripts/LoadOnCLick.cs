77c5bb9a84e62683c4269217bde52c84cdca2a81
Raphael Costa <rapha2706@hotmail.com>
2019-11-02 21:13:00 +0000

    Fazendo modos diferentes

---
+++ Unity project/Assets/Scripts/LoadOnCLick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnCLick : MonoBehaviour
+{
+    public void loadScene(int mode)
+    {
+        Application.LoadLevel(mode);
+    }
+}
